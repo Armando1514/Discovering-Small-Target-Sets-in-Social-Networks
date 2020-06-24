2a2930ede5828565c2e895b836c6d07ddfb37a23
Armando1514 <armando@ferrara.link>
2020-06-24 12:52:12 +0200

    Bug fixing
    bug fixing regarding threshold below degree.

--- TSSAlgorithm.py
+++ TSSAlgorithm.py
@@ -42,7 +42,8 @@ def update_formula(nodes):
 def threshold_reached(node_id):
     id_node_where_update_formula = []
     node_with_threshold_to_zero = [node_id]
-    
+    node_with_degree_below_threshold = []
+
     while len(node_with_threshold_to_zero) > 0:
         node_to_analyse = node_with_threshold_to_zero.pop()
         n = g.GetNI(node_to_analyse)
@@ -54,12 +55,17 @@ def threshold_reached(node_id):
             if node_out_threshold > 0:
                 node_out_threshold = node_out_threshold - 1
                 g.AddIntAttrDatN(node_out_id, node_out_threshold, "threshold")
-                if g.GetNI(node_out_id).GetInDeg() - 1 < 1:
-                    degree_below_threshold(node_out_id)
+                if g.GetNI(node_out_id).GetInDeg() - 1 < node_out_threshold:
+                    node_with_degree_below_threshold.append(node_out_id)
             else:
                 node_with_threshold_to_zero.append(node_out_id)
+
+    while len(node_with_degree_below_threshold) > 0:
+        node_pop = node_with_degree_below_threshold.pop()
+        if g.IsNode(node_pop):
+            degree_below_threshold(node_pop)
+
     g.DelNode(node_id)
-    print("threshold r %d" % node_id)
     del formula_table[node_id]
     update_formula(id_node_where_update_formula)
 
@@ -75,7 +81,6 @@ def degree_below_threshold(node_id):
         if g.IsNode(node_to_analyse):
             n = g.GetNI(node_to_analyse)
             for node_out_id in n.GetOutEdges():
-                print(g.IsNode(node_out_id))
 
                 id_node_where_update_formula.append(node_out_id)
                 node_out_threshold = g.GetIntAttrDatN(node_out_id, "threshold")
@@ -83,7 +88,7 @@ def degree_below_threshold(node_id):
                 g.AddIntAttrDatN(node_out_id, node_out_threshold, "threshold")
                 if node_out_threshold == 0:
                     threshold_reached(node_out_id)
-                elif g.GetNI(node_out_id).GetInDeg() - 1 < 1:
+                elif g.GetNI(node_out_id).GetInDeg() - 1 < node_out_threshold:
                     node_with_degree_below_to_zero.append(node_out_id)
         if g.IsNode(node_id):
             g.DelNode(node_id)
