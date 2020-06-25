989d15985502c881d00be87f5117cda474d4b682
Armando1514 <armando@ferrara.link>
2020-06-25 13:02:27 +0200

    Greedy TSS Fixed

--- GreedyTSSAlgorithm.py
+++ GreedyTSSAlgorithm.py
@@ -1,11 +1,9 @@
-import operator
-
 import GraphTools
 
 g = GraphTools.load_graph_from_txt("Slashdot0902.txt")
 print('Graph Nodes: %d, Edges: %d' % (g.GetNodes(), g.GetEdges()))
 
-g = GraphTools.deferred_decisions_with_uniform_probability(g)
+g =GraphTools.deferred_decisions_with_uniform_probability(g)
 
 g = GraphTools.constant_threshold_assignment(g, 2)
 
@@ -13,58 +11,65 @@ print('After Deferred decision Nodes: %d, Edges: %d' % (g.GetNodes(), g.GetEdges
 
 seed_set = []
 
-threshold_table = {}
-degree_table = {}
+nodes_table = {}
 
 
 def calculate_tables():
-
     for node in g.Nodes():
         n_id = node.GetId()
         n_threshold = g.GetIntAttrDatN(n_id, "threshold")
         n_out_degree = node.GetOutDeg()
-        threshold_table[n_id] = n_threshold
-        degree_table[n_id] = n_out_degree
+        nodes_table[n_id] = {"degree": n_out_degree, "threshold": n_threshold}
 
 
 def get_maximum_degree_node_id():
-    return max(degree_table.items(), key=operator.itemgetter(1))[0]
+    max_value = -1
+    max_node_id = 0
+    for node in nodes_table:
+        if nodes_table[node]["threshold"] > 0 and nodes_table[node]["degree"] > max_value:
+            max_value = nodes_table[node]["degree"]
+            max_node_id = node
+
+    return max_node_id
 
 
 def get_maximum_threshold_node_id():
-    return max(threshold_table.items(), key=operator.itemgetter(1))[0]
+    max_node_id = -1
+    for node in nodes_table:
+        if nodes_table[node]["threshold"] > 0:
+            max_node_id = node
+            break
+
+    if max_node_id == -1:
+        return next(iter(nodes_table))
+
+    return max_node_id
 
 
 calculate_tables()
 
 
 def update_nodes(x):
-    i = 0
-
     for out_node in g.GetNI(x).GetOutEdges():
 
-        out_threshold = g.GetIntAttrDatN(out_node, "threshold")
-        if out_threshold > 0:
-            i = i + 1
-        if degree_table[out_node] > 0:
-            degree_table[out_node] = degree_table[out_node] - 1
-        threshold_table[out_node] = threshold_table[out_node] - 1
+        if nodes_table[out_node]["degree"] > 0 and g.IsEdge(out_node, x):
+            nodes_table[out_node]["degree"] = nodes_table[out_node]["degree"] - 1
 
-    degree_table[x] = i
+        if nodes_table[out_node]["threshold"] > 0:
+            nodes_table[out_node]["threshold"] = nodes_table[out_node]["threshold"] - 1
 
 
 while g.GetNodes():
 
     v = get_maximum_threshold_node_id()
-
-    if threshold_table[v] > 0:
+    if nodes_table[v]["threshold"] > 0:
         v = get_maximum_degree_node_id()
         seed_set.append(v)
-        update_nodes(v)
 
+    update_nodes(v)
     g.DelNode(v)
 
-    del degree_table[v]
-    del threshold_table[v]
+    del nodes_table[v]
 
 print('Seed Set Length: ', len(seed_set))
+print("Seed Set:", seed_set)
