13b9c3b09c43a84a7e6390a064dc228cea1c11e8
Armando1514 <armando@ferrara.link>
2020-06-25 16:55:12 +0200

    GraphTools updated and dataset updated

--- GraphTools.py
+++ GraphTools.py
@@ -32,6 +32,7 @@ def deferred_decisions_with_proportional_to_the_degree(g):
         for rm in edge_to_remove:
             g.DelEdge(n.GetId(), rm)
             edge_to_remove.remove(rm)
+
     return g
 
 
@@ -45,19 +46,18 @@ def constant_threshold_assignment(g, constant_value):
 def proportional_to_the_degree_threshold_assignment(g):
     g = snap.ConvertGraph(snap.PNEANet, g)
     for n in g.Nodes():
-        g.AddIntAttrDatN(n.GetId(), n.GetDeg(), "threshold")
+        deg = n.GetDeg()
+        value = 5
+        if deg > 0:
+            value = int((1 /(deg + value)) * (g.GetEdges() / g.GetNodes()))
+        g.AddIntAttrDatN(n.GetId(), value, "threshold")
     return g
 
 
-def proportional_to_the_out_degree_threshold_assignment(g):
+def random_threshold_assignment(g):
     g = snap.ConvertGraph(snap.PNEANet, g)
     for n in g.Nodes():
-        g.AddIntAttrDatN(n.GetId(), n.GetOutDeg(), "threshold")
+        g.AddIntAttrDatN(n.GetId(), random.randint(1, 5), "threshold")
     return g
 
 
-def proportional_to_the_in_degree_threshold_assignment(g):
-    g = snap.ConvertGraph(snap.PNEANet, g)
-    for n in g.Nodes():
-        g.AddIntAttrDatN(n.GetId(), n.GetInDeg(), "threshold")
-    return g
